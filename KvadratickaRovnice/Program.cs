66a295d453f8409a7c582d94f160b63b53c244d1
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-03-06 07:54:47 +0100

    Přidejte soubory projektu.

---
+++ KvadratickaRovnice/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace KvadratickaRovnice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+            double D;
+            double x1, x2;
+
+            Console.Write("Zadej koeficient a: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Zadej koeficient b: ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.Write("Zadej koeficient c: ");
+            c = double.Parse(Console.ReadLine());
+
+            D = b * b - 4 * a * c;
+
+            Console.WriteLine($"Diskriminant = {D}");
+
+            if (D < 0 )
+            {
+                Console.WriteLine("Nemá řešení");
+            }
+            else if (D == 0)
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine($"x = {x1}");
+            }
+            else
+            {
+                x1 = (-b + Math.Sqrt(D))/(2*a);
+                x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                Console.WriteLine($"x1 = {x1}");
+                Console.WriteLine($"x2 = {x2}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
